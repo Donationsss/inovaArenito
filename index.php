279f5ea8f242031e9f39baded32baf74a25cfc6d
Donassan <enzo.donassan@gmail.com>
2025-10-16 10:21:06 -0300

    testando aqui

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ $username = $_SESSION['usuario_nome'] ?? 'Usuário';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TechStore - Loja Virtual Premium</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/carrinho.css">
-    <link rel="stylesheet" href="./css/out-of-stock.css">
+    <link rel="stylesheet" href="../inovaArenito/lojaVirtual/css/styles.css">
+    <link rel="stylesheet" href="../inovaArenito/lojaVirtual/css/carrinho.css">
+    <link rel="stylesheet" href="../inovaArenito/lojaVirtual/css/out-of-stock.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
